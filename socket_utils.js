e130bd8dc283c632480de18bca9d6a755fa1d214
phil-svg <31007753+phil-svg@users.noreply.github.com>
2023-02-14 11:16:48 +0100

    spring cleaning

--- socket_utils.js
+++ socket_utils.js
@@ -1,284 +1,260 @@
+const fs = require("fs");
 
-const fs = require("fs")
+const https = require("https");
 
-const https = require("https")
+const genericUtils = require("./generic_utils.js");
+const getCurvePools = genericUtils.getCurvePools;
 
-const generic_utils = require("./generic_utils.js")
-const getCurvePools = generic_utils.getCurvePools
-
-const balances_utils = require("./balances_utils.js")
-const readBalancesArray = balances_utils.readBalancesArray
+const balancesUtils = require("./balances_utils.js");
+const readBalancesArray = balancesUtils.readBalancesArray;
 
 // utils for price-data
-const price_utils = require("./price_utils.js")
-const readPriceArray = price_utils.readPriceArray
+const priceUtils = require("./price_utils.js");
+const readPriceArray = priceUtils.readPriceArray;
 
 // for the search bar on the landing page
-const search_utils = require("./search_utils.js")
-const search = search_utils.search
+const searchUtils = require("./search_utils.js");
+const search = searchUtils.search;
 
 // utils to fetch and store bonding curves
-const bondingCurve_utils = require("./bondingCurve_utils.js")
-const getBondingCurveForPoolAndCombination = bondingCurve_utils.getBondingCurveForPoolAndCombination
+const bondingCurveUtils = require("./bonding_curve_utils.js");
+const getBondingCurveForPoolAndCombination = bondingCurveUtils.getBondingCurveForPoolAndCombination;
 
-async function http_SocketSetup(Server,emitter,whiteListedPoolAddress) {
+async function httpSocketSetup(Server, emitter, whiteListedPoolAddress) {
+  const io = new Server(2424, {
+    cors: {
+      origin: "http://localhost:2424",
+      methods: ["GET", "POST"],
+    },
+  });
 
-	const io = new Server(2424, {
-		cors: {
-			origin: "http://localhost:2424",
-			methods: ["GET", "POST"]
-		}
-	})
-
-	await initSocketMessages(io,emitter,whiteListedPoolAddress)
-	await startLandingSocket(io)
+  await initSocketMessages(io, emitter, whiteListedPoolAddress);
+  await startLandingSocket(io);
 }
 
-async function https_SocketSetup(Server,emitter,whiteListedPoolAddress) {
+async function httpsSocketSetup(Server, emitter, whiteListedPoolAddress) {
+  const httpsServer = https.createServer({
+    key: fs.readFileSync("/home/transactions/certs/privkey1.pem"),
+    cert: fs.readFileSync("/home/transactions/certs/cert1.pem"),
+    ca: fs.readFileSync("/home/transactions/certs/fullchain1.pem"),
+  });
 
-	const httpsServer = https.createServer({
-		key: fs.readFileSync("/home/transactions/certs/privkey1.pem"),
-		cert: fs.readFileSync("/home/transactions/certs/cert1.pem"),
-		ca: fs.readFileSync("/home/transactions/certs/fullchain1.pem")
-	})
+  const io = new Server(httpsServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+    requestCert: false,
+    rejectUnauthorized: false,
+  });
 
-	const io = new Server(httpsServer, {
-		cors: {
-			origin: "*",
-			methods: ["GET", "POST"]
-		},
-		requestCert: false,
-		rejectUnauthorized: false,
-	})
-	
-	await initSocketMessages(io,emitter,whiteListedPoolAddress)
-	await startLandingSocket(io)
+  await initSocketMessages(io, emitter, whiteListedPoolAddress);
+  await startLandingSocket(io);
 
-	httpsServer.listen(2053)
+  httpsServer.listen(2053);
 }
 
-async function startLandingSocket(io){
-	const landing_socket = io.of("/" )
-	landing_socket.on("connection", async (socket) => {
-		console.log("landing_socket connected")
-		socket.on("search", (data) => {
-			try{
-				let res = search(data)
-				socket.emit("search_res", res)
-			}catch(err){
-				console.log("err in search:",err.message)
-			}
-		})
-		socket.on("ping", () => {
-			socket.emit("pong")
-		})
-		socket.on("disconnect", () => {
-			console.log("client disconnected from landing page")
-		})
-	})
+async function startLandingSocket(io) {
+  const LANDING_SOCKET = io.of("/");
+  LANDING_SOCKET.on("connection", async (socket) => {
+    console.log("landing_socket connected");
+    socket.on("search", (data) => {
+      try {
+        const res = search(data);
+        socket.emit("search_res", res);
+      } catch (err) {
+        console.log("err in search:", err.message);
+      }
+    });
+    socket.on("ping", () => {
+      socket.emit("pong");
+    });
+    socket.on("disconnect", () => {
+      console.log("client disconnected from landing page");
+    });
+  });
 }
 
 // making use of socket.io rooms
-async function manageUpdates(io,emitter,poolAddress){
-	emitter.on("Update Table-ALL" + poolAddress, async (data) => {
-		io.in(poolAddress).emit("Update Table-ALL",data)
-	})
-	emitter.on("Update Table-MEV" + poolAddress, async (data) => {
-		io.in(poolAddress).emit("Update Table-MEV",data)
-	})
-	emitter.on("Update Price-Chart" + poolAddress, async (unixtime) => {
-		io.in(poolAddress).emit("Update Price-Char",unixtime)
-	})
-	emitter.on("Update Balance-Chart" + poolAddress, async (data) => {
-		io.in(poolAddress).emit("Update Balance-Chart",data)
-	})
-	emitter.on("Update TVL-Chart" + poolAddress, async (data) => {
-		io.in(poolAddress).emit("Update TVL-ALL",data)
-	})
-	emitter.on("Update Volume-Chart" + poolAddress, async (data) => {
-		io.in(poolAddress).emit("Update Volume-Chart",data)
-	})
+async function manageUpdates(io, emitter, poolAddress) {
+  emitter.on("General Pool Update" + poolAddress, async (data) => {
+    io.in(poolAddress).emit("Update Table-ALL", data.all);
+    io.in(poolAddress).emit("Update Price-Chart", data.unixtime);
+    io.in(poolAddress).emit("Update Balance-Chart", data.balances);
+    io.in(poolAddress).emit("Update TVL-ALL", data.tvl);
+    io.in(poolAddress).emit("Update Volume-Chart", data.volume);
+  });
+
+  emitter.on("Update Table-MEV" + poolAddress, async (data) => {
+    io.in(poolAddress).emit("Update Table-MEV", data);
+  });
 }
 
 /**
- * on pool connect: 
+ * on pool connect:
  * send table data full (so one month)
  * send data cut for 1 month for: price chart (later balances and more)
  * then socket.on("timePeriod") => send data for: price chart (later balances and more)
- */ 
+ */
 
-async function initSocketMessages(io,emitter,whiteListedPoolAddress){
-	let pools = getCurvePools()
-	for (const poolAddress of pools) {
-		if (poolAddress!== whiteListedPoolAddress) continue
-		const pool_socket = io.of("/" + poolAddress)
+async function initSocketMessages(io, emitter, whiteListedPoolAddress) {
+  const POOLS = getCurvePools();
+  for (const POOL_ADDRESS of POOLS) {
+    if (POOL_ADDRESS !== whiteListedPoolAddress) continue;
+    const POOL_SOCKET = io.of("/" + POOL_ADDRESS);
 
-		await manageUpdates(pool_socket,emitter,poolAddress)
+    await manageUpdates(POOL_SOCKET, emitter, POOL_ADDRESS);
 
-		pool_socket.on("connection", async (socket) => {
-			socket.join(poolAddress)
-			socket.send("successfully connected to socket for " + poolAddress)
-			console.log(poolAddress, "socket connected")
+    POOL_SOCKET.on("connection", async (socket) => {
+      socket.join(POOL_ADDRESS);
+      socket.send("successfully connected to socket for " + POOL_ADDRESS);
+      console.log(POOL_ADDRESS, "socket connected");
 
-			//sending the array of token names, used in the price chart switch (priceOf: [...] priceIn: [...] on the UI)
-			let curveJSON = JSON.parse(fs.readFileSync("CurvePoolData.json"))
-			let coin_names = curveJSON[poolAddress].coin_names
-			socket.emit("token names inside pool", coin_names)
+      // sending the array of token names, used in the price chart switch (priceOf: [...] priceIn: [...] on the UI)
+      const CURVE_JSON = JSON.parse(fs.readFileSync("curve_pool_data.json"));
+      const COIN_NAMES = CURVE_JSON[POOL_ADDRESS].coin_names;
+      socket.emit("token names inside pool", COIN_NAMES);
 
-			// eg [ 'sUSD', 'DAI' ] => price of sUSD in DAI
-			let price_combination = [coin_names[coin_names.length-1],coin_names[0]]
-			if (poolAddress == "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD") price_combination = [ 'sUSD', 'USDC' ]
+      // eg [ 'sUSD', 'DAI' ] => price of sUSD in DAI
+      let priceCombination = [COIN_NAMES[COIN_NAMES.length - 1], COIN_NAMES[0]];
+      if (POOL_ADDRESS === "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD") priceCombination = ["sUSD", "USDC"];
 
-			// needed for when a user changes coins in the price chart, to keep track in which 
-			let timeFrame = "month"
+      // needed for when a user changes coins in the price chart, to keep track in which
+      let timeFrame = "month";
 
-			socket.on("priceOf", (priceOf) => {
-				price_combination[0] = priceOf
-				//socket.emit("search_res", res)
-			})
+      socket.on("priceOf", (priceOf) => {
+        priceCombination[0] = priceOf;
+        // socket.emit("search_res", res)
+      });
 
-			socket.on("priceIn", (priceIn) => {
-				price_combination[1] = priceIn
-				//socket.emit("search_res", res)
-			})
+      socket.on("priceIn", (priceIn) => {
+        priceCombination[1] = priceIn;
+        // socket.emit("search_res", res)
+      });
 
-			// messages on connect
-			sendTableData(socket,poolAddress)
-			sendPriceData(timeFrame,socket,poolAddress,price_combination)
-			sendBalanceData(timeFrame,socket,poolAddress)
-			sendVolumeData(timeFrame,socket,poolAddress)
-			sendTVLData(timeFrame,socket,poolAddress)
-			sendBondingCurve(socket,poolAddress,price_combination)
+      // messages on connect
+      sendTableData(socket, POOL_ADDRESS);
+      sendPriceData(timeFrame, socket, POOL_ADDRESS, priceCombination);
+      sendBalanceData(timeFrame, socket, POOL_ADDRESS);
+      sendVolumeData(timeFrame, socket, POOL_ADDRESS);
+      sendTVLData(timeFrame, socket, POOL_ADDRESS);
+      sendBondingCurve(socket, POOL_ADDRESS, priceCombination);
 
-			// next block is for when a user plays with the time-span tabulator
-			socket.on("day", () => {
-				timeFrame = "day"
-				sendPriceData(timeFrame,socket,poolAddress,price_combination)
-				sendBalanceData(timeFrame,socket,poolAddress)
-				sendVolumeData(timeFrame,socket,poolAddress)
-				sendTVLData(timeFrame,socket,poolAddress)
-			})
-			socket.on("week", () => {
-				timeFrame = "week"
-				sendPriceData(timeFrame,socket,poolAddress,price_combination)
-				sendBalanceData(timeFrame,socket,poolAddress)
-				sendVolumeData(timeFrame,socket,poolAddress)
-				sendTVLData(timeFrame,socket,poolAddress)
-			})
-			socket.on("month", () => {
-				timeFrame = "month"
-				sendPriceData(timeFrame,socket,poolAddress,price_combination)
-				sendBalanceData(timeFrame,socket,poolAddress)
-				sendVolumeData(timeFrame,socket,poolAddress)
-				sendTVLData(timeFrame,socket,poolAddress)
-			})
-			
-			socket.on("disconnect", () => {
-				console.log("client disconnected")
-			})
+      // next block is for when a user plays with the time-span tabulator
+      socket.on("day", () => {
+        timeFrame = "day";
+        sendPriceData(timeFrame, socket, POOL_ADDRESS, priceCombination);
+        sendBalanceData(timeFrame, socket, POOL_ADDRESS);
+        sendVolumeData(timeFrame, socket, POOL_ADDRESS);
+        sendTVLData(timeFrame, socket, POOL_ADDRESS);
+      });
+      socket.on("week", () => {
+        timeFrame = "week";
+        sendPriceData(timeFrame, socket, POOL_ADDRESS, priceCombination);
+        sendBalanceData(timeFrame, socket, POOL_ADDRESS);
+        sendVolumeData(timeFrame, socket, POOL_ADDRESS);
+        sendTVLData(timeFrame, socket, POOL_ADDRESS);
+      });
+      socket.on("month", () => {
+        timeFrame = "month";
+        sendPriceData(timeFrame, socket, POOL_ADDRESS, priceCombination);
+        sendBalanceData(timeFrame, socket, POOL_ADDRESS);
+        sendVolumeData(timeFrame, socket, POOL_ADDRESS);
+        sendTVLData(timeFrame, socket, POOL_ADDRESS);
+      });
 
-		})
-	}
+      socket.on("disconnect", () => {
+        console.log("client disconnected");
+      });
+    });
+  }
 }
 
 // sends the inital data for the table-view (tx history for a given pool)
-function sendTableData(socket,poolAddress){
-	let currentTime = new Date().getTime() / 1000
-	var days = 31
-	let startingPoint = currentTime - (days * 24 * 60 * 60)
+function sendTableData(socket, poolAddress) {
+  const CURRENT_TIME = new Date().getTime() / 1000;
+  const DAYS = 31;
+  const STARTING_POINT = CURRENT_TIME - DAYS * 24 * 60 * 60;
 
-	let dataALL = JSON.parse(fs.readFileSync("processedTxLog_ALL.json"))
-	let dataMEV = JSON.parse(fs.readFileSync("processedTxLog_MEV.json"))
+  const DATA_ALL = JSON.parse(fs.readFileSync("processed_tx_log_all.json"));
+  const DATA_MEV = JSON.parse(fs.readFileSync("processed_tx_log_mev.json"));
 
-	let trimmedDataALL = dataALL[poolAddress].filter(entry => entry.unixtime >= startingPoint)
-	let trimmedDataMEV = dataMEV[poolAddress].filter(entry => entry.unixtime >= startingPoint)
+  const TRIMMED_DATA_ALL = DATA_ALL[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
+  const TRIMMED_DATA_MEV = DATA_MEV[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
 
-	socket.emit("table_all", trimmedDataALL)
-	socket.emit("table_mev", trimmedDataMEV)
+  socket.emit("table_all", TRIMMED_DATA_ALL);
+  socket.emit("table_mev", TRIMMED_DATA_MEV);
 }
 
 // trimmes down the message for the frontend to ship only data of last 24h, week, or month
-function sendPriceData(timeFrame,socket,poolAddress,price_combination){
-	let currentTime = new Date().getTime() / 1000
+function sendPriceData(timeFrame, socket, poolAddress, priceCombination) {
+  const STARTING_POINT = getStartingPoint(timeFrame);
 
-	if (timeFrame == "day") var days = 1
-	if (timeFrame == "week") var days = 7
-	if (timeFrame == "month") var days = 31
+  const PRICE_OF = priceCombination[0];
+  const PRICE_IN = priceCombination[1];
+  const DATA = readPriceArray(poolAddress, PRICE_OF, PRICE_IN);
 
-	let startingPoint = currentTime - (days * 24 * 60 * 60)
-
-	let priceOf = price_combination[0]
-	let priceIn = price_combination[1]
-	let data = readPriceArray(poolAddress,priceOf,priceIn)
-
-	let trimmedData = data.filter(item => Object.keys(item)[0] >= startingPoint)
-	socket.emit("price_chart_combination", price_combination)
-	socket.emit("price_chart", trimmedData)
+  const TRIMMED_DATA = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+  socket.emit("price_chart_combination", priceCombination);
+  socket.emit("price_chart", TRIMMED_DATA);
 }
 
-function sendBalanceData(timeFrame,socket,poolAddress){
-	let currentTime = new Date().getTime() / 1000
-
-	if (timeFrame == "day") var days = 1
-	if (timeFrame == "week") var days = 7
-	if (timeFrame == "month") var days = 31
-
-	let startingPoint = currentTime - (days * 24 * 60 * 60)
-
-	let data = readBalancesArray(poolAddress)
-
-	let trimmedData = data.filter(item => Object.keys(item)[0] >= startingPoint)
-	socket.emit("balances_chart", trimmedData)
+function getStartingPoint(timeFrame) {
+  const CURRENT_TIME = new Date().getTime() / 1000;
+  let days;
+  if (timeFrame === "day") days = 1;
+  if (timeFrame === "week") days = 7;
+  if (timeFrame === "month") days = 31;
+  return CURRENT_TIME - days * 24 * 60 * 60;
 }
 
-function sendVolumeData(timeFrame,socket,poolAddress){
-	let currentTime = new Date().getTime() / 1000
-	if (timeFrame == "day") var days = 1
-	if (timeFrame == "week") var days = 7
-	if (timeFrame == "month") var days = 31
-	let startingPoint = currentTime - (days * 24 * 60 * 60)
+function sendBalanceData(timeFrame, socket, poolAddress) {
+  const STARTING_POINT = getStartingPoint(timeFrame);
 
-	// first getting trimmed processedTxLog
-	let dataALL = JSON.parse(fs.readFileSync("processedTxLog_ALL.json"))
-	let trimmedDataALL = dataALL[poolAddress].filter(entry => entry.unixtime >= startingPoint)
+  const DATA = readBalancesArray(poolAddress);
 
-	// then making vol-arr out of it
-	let data =  []
-	for(const entry of trimmedDataALL) {
-		let vol = entry.tradeDetails.valueUSD
-		if(!vol) vol = entry.tradeDetails[0].valueUSD
-		data.push({[entry.unixtime]:vol})
-	}
-	socket.emit("volume_chart", data)
+  const TRIMMED_DATA = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+  socket.emit("balances_chart", TRIMMED_DATA);
 }
 
-function sendTVLData(timeFrame,socket,poolAddress){
-    let currentTime = new Date().getTime() / 1000
+function sendVolumeData(timeFrame, socket, poolAddress) {
+  const STARTING_POINT = getStartingPoint(timeFrame);
 
-    if (timeFrame == "day") var days = 1
-    if (timeFrame == "week") var days = 7
-    if (timeFrame == "month") var days = 31
+  // first getting trimmed processedTxLog
+  const DATA_ALL = JSON.parse(fs.readFileSync("processed_tx_log_all.json"));
+  const TRIMMED_DATA_ALL = DATA_ALL[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
 
-    let startingPoint = currentTime - (days * 24 * 60 * 60)
-
-    let balances = readBalancesArray(poolAddress)
-    let trimmedBalances = balances.filter(item => Object.keys(item)[0] >= startingPoint)
-
-    let data = trimmedBalances.map(obj => {
-        const key = Object.keys(obj)[0]
-        const sum = obj[key].reduce((a, b) => a + b, 0)
-        return { [key]: sum }
-    })
-
-    socket.emit("tvl_chart", data)
+  // then making vol-arr out of it
+  const DATA = [];
+  for (const ENTRY of TRIMMED_DATA_ALL) {
+    let vol = ENTRY.tradeDetails.valueUSD;
+    if (!vol && vol !== 0) vol = ENTRY.tradeDetails[0].valueUSD;
+    DATA.push({ [ENTRY.unixtime]: vol });
+  }
+  socket.emit("volume_chart", DATA);
 }
 
-function sendBondingCurve(socket,poolAddress,price_combination){
-	let bondingCurve = getBondingCurveForPoolAndCombination(poolAddress,price_combination)
-	socket.emit("bonding_curve", bondingCurve)
+function sendTVLData(timeFrame, socket, poolAddress) {
+  const STARTING_POINT = getStartingPoint(timeFrame);
+
+  const BALANCES = readBalancesArray(poolAddress);
+  const TRIMMED_BALANCES = BALANCES.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+
+  const DATA = TRIMMED_BALANCES.map((obj) => {
+    const KEY = Object.keys(obj)[0];
+    const SUM = obj[KEY].reduce((a, b) => a + b, 0);
+    return { [KEY]: SUM };
+  });
+
+  socket.emit("tvl_chart", DATA);
+}
+
+function sendBondingCurve(socket, poolAddress, priceCombination) {
+  const BONDING_CURVE = getBondingCurveForPoolAndCombination(poolAddress, priceCombination);
+  socket.emit("bonding_curve", BONDING_CURVE);
 }
 
 module.exports = {
-    http_SocketSetup,
-    https_SocketSetup
-}
+  httpSocketSetup,
+  httpsSocketSetup,
+};
