7e0b10c5cbd71f1bacc8a3a83a23dd20b8435d85
phil-svg <31007753+phil-svg@users.noreply.github.com>
2023-03-17 02:58:29 +0100

    capped messages to 1k entries for frontend

--- Utils/SocketUtils.mjs
+++ Utils/SocketUtils.mjs
@@ -108,7 +108,7 @@ function sendDefaultInitMessage(POOL_SOCKET, POOL_ADDRESS, timeFrame, priceCombi
 async function initSocketMessages(io, emitter, whiteListedPoolAddress) {
   const POOLS = getCurvePools();
   for (const POOL_ADDRESS of POOLS) {
-    if (POOL_ADDRESS !== whiteListedPoolAddress) continue;
+    // if (POOL_ADDRESS !== whiteListedPoolAddress) continue;
     const POOL_SOCKET = io.of("/" + POOL_ADDRESS);
 
     const CURVE_JSON = JSON.parse(fs.readFileSync("./JSON/CurvePoolData.json"));
@@ -144,14 +144,17 @@ async function initSocketMessages(io, emitter, whiteListedPoolAddress) {
 
       // next block is for when a user plays with the time-span tabulator
       socket.on("day", () => {
+        console.log("received request for timeframe: day");
         sendData("day", socket, POOL_ADDRESS, priceCombination);
       });
 
       socket.on("week", () => {
+        console.log("received request for timeframe: week");
         sendData("week", socket, POOL_ADDRESS, priceCombination);
       });
 
       socket.on("month", () => {
+        console.log("received request for timeframe: month");
         sendData("month", socket, POOL_ADDRESS, priceCombination);
       });
 
@@ -178,11 +181,15 @@ function sendTableData(socket, poolAddress) {
   const DATA_ALL = JSON.parse(fs.readFileSync("./JSON/ProcessedTxLogAll.json"));
   const DATA_MEV = JSON.parse(fs.readFileSync("./JSON/ProcessedTxLogMEV.json"));
 
-  const TRIMMED_DATA_ALL = DATA_ALL[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
-  const TRIMMED_DATA_MEV = DATA_MEV[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
+  let trimmedDataALL = DATA_ALL[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
+  let trimmedDataMEV = DATA_MEV[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
 
-  socket.emit("table_all", TRIMMED_DATA_ALL);
-  socket.emit("table_mev", TRIMMED_DATA_MEV);
+  const NUMBER_OF_MAX_ENTRIES = 1000;
+  if (trimmedDataALL.length > NUMBER_OF_MAX_ENTRIES) trimmedDataALL = trimmedDataALL.slice(-NUMBER_OF_MAX_ENTRIES);
+  if (trimmedDataMEV.length > NUMBER_OF_MAX_ENTRIES) trimmedDataMEV = trimmedDataMEV.slice(-trimmedDataMEV);
+
+  socket.emit("table_all", trimmedDataALL);
+  socket.emit("table_mev", trimmedDataMEV);
 }
 
 // trimmes down the message for the frontend to ship only data of last 24h, week, or month
@@ -193,9 +200,10 @@ function sendPriceData(timeFrame, socket, poolAddress, priceCombination) {
   const PRICE_IN = priceCombination[1];
   const DATA = readPriceArray(poolAddress, PRICE_OF, PRICE_IN);
 
-  const TRIMMED_DATA = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+  let trimmedData = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+  trimmedData = filterData(trimmedData);
   socket.emit("price_chart_combination", priceCombination);
-  socket.emit("price_chart", TRIMMED_DATA);
+  socket.emit("price_chart", trimmedData);
 }
 
 function getStartingPoint(timeFrame) {
@@ -212,8 +220,22 @@ function sendBalanceData(timeFrame, socket, poolAddress) {
 
   const DATA = readBalancesArray(poolAddress);
 
-  const TRIMMED_DATA = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
-  socket.emit("balances_chart", TRIMMED_DATA);
+  let trimmedData = DATA.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
+  trimmedData = filterData(trimmedData);
+  socket.emit("balances_chart", trimmedData);
+}
+
+function compressVolChart(DATA) {
+  const CHUNK_SIZE = Math.ceil(DATA.length / 1000);
+  const COMPRESSED = [];
+  for (let i = 0; i < DATA.length; i += CHUNK_SIZE) {
+    const CHUNK = DATA.slice(i, i + CHUNK_SIZE);
+    const CENTER_INDEX = Math.floor(CHUNK.length / 2);
+    const CENTER_TIME = Object.keys(CHUNK[CENTER_INDEX])[0];
+    const VOLUME = +CHUNK.reduce((acc, trade) => acc + Object.values(trade)[0], 0).toFixed(0);
+    COMPRESSED.push({ [CENTER_TIME]: VOLUME });
+  }
+  return COMPRESSED;
 }
 
 function sendVolumeData(timeFrame, socket, poolAddress) {
@@ -224,13 +246,14 @@ function sendVolumeData(timeFrame, socket, poolAddress) {
   const TRIMMED_DATA_ALL = DATA_ALL[poolAddress].filter((entry) => entry.unixtime >= STARTING_POINT);
 
   // then making vol-arr out of it
-  const DATA = [];
+  let data = [];
   for (const ENTRY of TRIMMED_DATA_ALL) {
     let vol = ENTRY.tradeDetails.valueUSD;
     if (!vol && vol !== 0) vol = ENTRY.tradeDetails[0].valueUSD;
-    DATA.push({ [ENTRY.unixtime]: vol });
+    data.push({ [ENTRY.unixtime]: vol });
   }
-  socket.emit("volume_chart", DATA);
+  data = compressVolChart(data);
+  socket.emit("volume_chart", data);
 }
 
 function sendTVLData(timeFrame, socket, poolAddress) {
@@ -239,18 +262,57 @@ function sendTVLData(timeFrame, socket, poolAddress) {
   const BALANCES = readBalancesArray(poolAddress);
   const TRIMMED_BALANCES = BALANCES.filter((item) => Object.keys(item)[0] >= STARTING_POINT);
 
-  const DATA = TRIMMED_BALANCES.map((obj) => {
+  let data = TRIMMED_BALANCES.map((obj) => {
     const KEY = Object.keys(obj)[0];
     const SUM = obj[KEY].reduce((a, b) => a + b, 0);
     return { [KEY]: SUM };
   });
 
-  socket.emit("tvl_chart", DATA);
+  data = filterData(data);
+  socket.emit("tvl_chart", data);
 }
 
 function sendBondingCurve(socket, poolAddress, priceCombination) {
   const BONDING_CURVE = getBondingCurveForPoolAndCombination(poolAddress, priceCombination);
   socket.emit("bonding_curve", BONDING_CURVE);
+}
+
+// shinks arr-size to 1000 by removing entries close by time-wise. Done to improve loading time on the UI.
+function filterData(data) {
+  const NUM_TO_KEEP = 1000;
+  if (data.length <= NUM_TO_KEEP) {
+    return data;
+  }
+  const distances = [];
+  for (let i = 0; i < data.length; i++) {
+    const CURRENT = Object.keys(data[i])[0];
+    const PREV = i > 0 ? Object.keys(data[i - 1])[0] : null;
+    const NEXT = i < data.length - 1 ? Object.keys(data[i + 1])[0] : null;
+    const PREV_DIST = PREV ? Math.abs(CURRENT - PREV) : Infinity;
+    const NEXT_DIST = NEXT ? Math.abs(NEXT - CURRENT) : Infinity;
+    const MIN_DIST = Math.min(PREV_DIST, NEXT_DIST);
+    distances.push({ INDEX: i, DISTANCE: MIN_DIST });
+  }
+  distances.sort((a, b) => b.DISTANCE - a.DISTANCE);
+  const INDICES_TO_KEEP = new Set();
+  INDICES_TO_KEEP.add(distances[0].INDEX);
+  let i = 1;
+  while (INDICES_TO_KEEP.size < NUM_TO_KEEP && i < distances.length) {
+    const CURRENT_INDEX = distances[i].INDEX;
+    const PREV_INDEX = CURRENT_INDEX > 0 ? distances[i - 1].INDEX : null;
+    const NEXT_INDEX = CURRENT_INDEX < data.length - 1 ? distances[i + 1].INDEX : null;
+    const PREV_DIST = PREV_INDEX ? Math.abs(CURRENT_INDEX - PREV_INDEX) : Infinity;
+    const NEXT_DIST = NEXT_INDEX ? Math.abs(NEXT_INDEX - CURRENT_INDEX) : Infinity;
+    if (PREV_DIST >= NEXT_DIST) {
+      INDICES_TO_KEEP.add(CURRENT_INDEX);
+    }
+    i++;
+  }
+  const FILTERED_DATA = [];
+  INDICES_TO_KEEP.forEach((index) => {
+    FILTERED_DATA.push(data[index]);
+  });
+  return FILTERED_DATA;
 }
 
 export { httpSocketSetup, httpsSocketSetup };
